14907db6c54bd8af4e10bf8dccf93695a10494f4
Hpipone <maherjojo125@gmail.com>
2024-04-02 16:16:43 +0700

    3rd Changes committed

--- sc.js
+++ sc.js
@@ -79,12 +79,3 @@ function scrollToTop() {
     top: 0,
   });
 }
-
-window.addEventListener("scroll", function () {
-  var scrollTopButton = document.querySelector(".scroll-top");
-  if (this.window.pageYOffset > 200) {
-    scrollTopButton.style.display = "block";
-  } else {
-    scrollTopButton.style.display = "none";
-  }
-});
